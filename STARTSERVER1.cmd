d350bbd68917c0920bf141d737bd845c76a20a8c
Kylie <50227534+LoveFistRP@users.noreply.github.com>
2019-07-10 19:09:53 -0400

    start

--- STARTSERVER1.cmd
+++ STARTSERVER1.cmd
@@ -1,3 +1,3 @@
-title WL 1
-cd /d C:\Users\admin\Desktop\poglist1\server-data
-C:\Users\admin\Desktop\poglist1\run.cmd +set onesync_enabled 1 +exec server1.cfg
+title DEVELOPER
+cd /d C:\Users\Admin\Documents\GitHub\lovefist
+C:\Users\Admin\Documents\GitHub\run.cmd +set onesync_enabled 1 +exec dev.cfg +exec mysql.cfg
