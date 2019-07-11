4cc5d5a9f21ca365e7eaa32ead906e3049e55c99
Kylie <50227534+LoveFistRP@users.noreply.github.com>
2019-07-11 01:01:50 -0400

    smiles

--- STARTSERVER1.cmd
+++ STARTSERVER1.cmd
@@ -1,3 +1,3 @@
 title DEVELOPER
-cd /d C:\Users\Admin\Documents\GitHub\lovefist
-C:\Users\Admin\Documents\GitHub\run.cmd +set onesync_enabled 1 +exec dev.cfg +exec mysql.cfg
+cd /d C:\Users\%USERNAME%\Documents\GitHub\lovefist
+C:\Users\%USERNAME%\Documents\GitHub\run.cmd +set onesync_enabled 1 +exec dev.cfg +exec mysql.cfg
