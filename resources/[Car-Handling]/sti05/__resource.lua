183c45fc6171c90d721c4e1193fc4fc77cac0e97
mikevskater <mikevskater@hotmail.com>
2019-07-16 10:00:48 -0400

    Fixed the sti05 for REAL this time

--- resources/[Car-Handling]/sti05/__resource.lua
+++ resources/[Car-Handling]/sti05/__resource.lua
@@ -5,13 +5,11 @@ files {
     'carvariations.meta',
     'carcols.meta',
     'handling.meta',
-	'vehiclelayouts.meta',
 }
 
 data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
 
 client_script 'vehicle_name.lua'
