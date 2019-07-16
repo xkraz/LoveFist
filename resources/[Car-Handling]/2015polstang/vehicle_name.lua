85de8bf84b475845fffc4e2a8dd28d3ea00feab2
mikevskater <mikevskater@hotmail.com>
2019-07-16 03:19:54 -0400

    2015 pol buff and name add.

---
+++ resources/[Car-Handling]/2015polstang/vehicle_name.lua
@@ -0,0 +1,9 @@
+
+
+
+
+Citizen.CreateThread(function()
+
+	AddTextEntry('2015polstang', 'Police Speed Enforcer')
+
+end)
--- resources/[Car-Handling]/brzrbv3/__resource.lua
+++ resources/[Car-Handling]/brzrbv3/__resource.lua
@@ -14,4 +14,4 @@ data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'   -- Not Required
 
---client_script 'vehicle_name.lua'
+client_script 'vehicle_name.lua'
