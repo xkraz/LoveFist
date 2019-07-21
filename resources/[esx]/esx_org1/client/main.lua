ace7940a70967525d02fd31e1274d2e1482197bf
LoveFistRP <lovefistrp@gmail.com>
2019-07-21 01:44:02 -0700

    ORG1

--- resources/[esx]/esx_org1/client/main.lua
+++ resources/[esx]/esx_org1/client/main.lua
@@ -1123,7 +1123,7 @@ Citizen.CreateThread(function()
 
 		Citizen.Wait(0)
 
-		if PlayerData.job ~= nil and PlayerData.job.name == 'org1' or PlayerData.job.name == 'police' then
+		if PlayerData.job ~= nil and PlayerData.job.name == 'org1' or PlayerData.job.name == 'police'  then
 
 			local playerPed = PlayerPedId()
 			local coords    = GetEntityCoords(playerPed)
