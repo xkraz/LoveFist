9fac8111e6cef475280635c9d839d3ec5616c76b
DudeRock <50227534+LoveFistRP@users.noreply.github.com>
2019-07-22 20:11:07 -0300

    Changed PDM impound location
    New location for mechanics to impound vehicles

--- resources/[utility]/JAM/JAM_VehicleFinance/JAM_VehicleFinance_Config.lua
+++ resources/[utility]/JAM/JAM_VehicleFinance/JAM_VehicleFinance_Config.lua
@@ -7,5 +7,5 @@ JVF.FinanceStartingAmount = 10
 JVF.MaxRepayTime = 24 -- hours
 JVF.TowDriverJob = "mechanic"
 JVF.TowDriverLabel = "Experienced"
-JVF.RepoPoint = vector3(-44.28, -1083.45, 25.50)
+JVF.RepoPoint = vector3(744.1, -214.42, 73.64)
 JVF.MaxRepayments = 10
