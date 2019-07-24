3e800c036d6066cba91b5a26c65e3dc031856e44
DudeRock <50227534+LoveFistRP@users.noreply.github.com>
2019-07-24 16:48:48 -0300

    Changed Casino Boss Menus and such

--- resources/program-casino/config.lua
+++ resources/program-casino/config.lua
@@ -13,25 +13,25 @@ Config.MissCraft                  = 10 -- %
 Config.CasinoSites = {
 
 	CasinoActions = {
-		Pos   = { x = 924.69, y = -941.53, z = 44.75 },
+		Pos   = { x = 1109.73, y = 242.00, z = -45.83 },
 		Size  = { x = 1.5, y = 1.5, z = 1.5 },
 		Color = { r = 204, g = 204, b = 0 },
 		Type  = 22,
 	},
 	CasinoCloak = {
-		Pos   = { x = 946.88, y = -954.53, z = 44.0 },
+		Pos   = { x = 1119.38, y = 262.07, z = -51.04 },
 		Size  = { x = 0.8, y = 0.8, z = 0.8 },
 		Color = { r = 110, g = 110, b = 244 },
 		Type  = 24,
 	},
 	CasinoBar = {
-		Pos	  = { x = 910.55,   y = -962.72, z = 44.780},
+		Pos	  = { x = 1110.17,   y = 208.27, z = -49.44},
 		Size  = { x = 1.0, y = 1.0, z = 1.0 },
 		Color = { r = 204, g = 204, b = 0 },
 		Type  = 22,
 	},
 	CasinoFridge = {
-		Pos	  = { x = 910.97,   y = -959.71, z = 44.780},
+		Pos	  = { x = 1113.69,   y = 207.61, z = -49.44},
 		Size  = { x = 1.0, y = 1.0, z = 1.0 },
 		Color = { r = 204, g = 204, b = 0 },
 		Type  = 22,
