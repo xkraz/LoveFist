4daf461b11db52c99d71a4b72a95c987219ca61e
DudeRock <50227534+LoveFistRP@users.noreply.github.com>
2019-07-31 15:45:36 -0300

    Adding Voting system for Elections

---
+++ resources/esx_vote/config.lua
@@ -0,0 +1,37 @@
+Config                           = {}
+Config.DrawDistance              = 100.0
+Config.MarkerColor               = {r = 39, g = 131, b = 206}
+
+Config.Zones = {
+
+	VoteBooth1 = {
+		MarkerPos  = {x = -266.36416625977, y = -2035.6640625, z = 29.145593643188 },
+		Size       = {x = 1.5, y = 1.5, z = 1.0},
+		Type       = 1
+	},
+	
+
+	VoteBooth2 = {
+		MarkerPos  = {x = 238.06, y = -412.25, z = 47.11 },
+		Size       = {x = 1.5, y = 1.5, z = 1.0},
+		Type       = 1
+	},
+	
+	VoteBooth3 = {
+		MarkerPos  = {x = -267.28, y = -971.88, z = 30.22 },
+		Size       = {x = 1.5, y = 1.5, z = 1.0},
+		Type       = 1
+	},
+	
+	VoteBooth4 = {
+		MarkerPos  = {x = 437.68, y = -979.35, z = 29.69 },
+		Size       = {x = 1.5, y = 1.5, z = 1.0},
+		Type       = 1
+	},
+	
+	VoteBooth5 = {
+		MarkerPos  = {x = -266.36416625977, y = -2035.6640625, z = 29.145593643188 },
+		Size       = {x = 1.5, y = 1.5, z = 1.0},
+		Type       = 1
+	},
+}
