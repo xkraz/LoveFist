047f66c9f31eb85570bcc6e5a5fc964f3e81e416
Salvo <52991708+DatBoiSalvo@users.noreply.github.com>
2019-08-09 21:07:03 -0500

    Spawn with hat in locker room
    Didn't mean to do one by one, my memory is shit

--- resources/[esx]/esx_policejob/config.lua
+++ resources/[esx]/esx_policejob/config.lua
@@ -726,7 +726,7 @@ Config.Uniforms = {
 			['arms'] = 11,
 			['pants_1'] = 25,   ['pants_2'] = 1,
 			['shoes_1'] = 25,   ['shoes_2'] = 0,
-			['helmet_1'] = -1,  ['helmet_2'] = 0,
+			['helmet_1'] = 0,  ['helmet_2'] = 0,
 			['chain_1'] = 0,    ['chain_2'] = 0,
 			['ears_1'] = 2,     ['ears_2'] = 0
 		},
