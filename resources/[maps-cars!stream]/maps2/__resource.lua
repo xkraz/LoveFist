bf15e82a63ef9c0e2b9a6545f92de5a2b15b234d
MotoDestructo <rikki.knight@onthelevelgames.com>
2019-08-30 09:59:32 -0500

    Map cleanup
    Map cleanup

--- resources/[maps-cars!stream]/maps2/__resource.lua
+++ resources/[maps-cars!stream]/maps2/__resource.lua
@@ -3,7 +3,7 @@ this_is_a_map 'yes'
 
 file 'v_int_40.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/v_int_40.ytyp'
--- data_file 'DLC_ITYP_REQUEST' 'stream/lafa2k_modernhouse.ytyp' --file in another folder
--- data_file 'DLC_ITYP_REQUEST' 'stream/yakuza.ytyp' --file in another folder
+data_file 'DLC_ITYP_REQUEST' 'stream/lafa2k_modernhouse.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/yakuza.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/builderdef.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/builderdefaddons.ytyp'
