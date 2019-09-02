2a0a1317682478cd760378611874e90338df8deb
MotoDestructo <rikki.knight@onthelevelgames.com>
2019-09-02 11:34:25 -0500

    Moved aircraft shop for Riggs Flight School
    Moved aircraft shop for Riggs Flight School

--- resources/[esx]/esx_aircraftshop/config.lua
+++ resources/[esx]/esx_aircraftshop/config.lua
@@ -18,7 +18,7 @@ Config.PlateUseSpace = true
 Config.Zones = {
 
 	ShopEntering = { -- Marker for Accessing Shop
-		Pos   = { x = -965.13, y = -2929.40, z = 12.95 },
+		Pos   = { x = -936.77, y = -3033.16, z = 12.44 },
 		Size  = { x = 1.5, y = 1.5, z = 1.0 },
 		Type  = 1
 	},
