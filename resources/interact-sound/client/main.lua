24656f6fed92ed3b11afd987692c6bec5947d211
mikevskater <mikevskater@hotmail.com>
2019-09-06 01:03:21 -0400

    Michaels First no-internet dump (95% bug free)

--- resources/interact-sound/client/main.lua
+++ resources/interact-sound/client/main.lua
@@ -1,3 +1,82 @@
+-- ##############################################################
+-- #                                                            #
+-- # IF YOU ADD A SOUND IT MUST BE IN OGG FORMAT                #
+-- # ADD TO THE THREE ARRAYS BELOW THE NAME OF THE AUDIO FILE   #
+-- # _sounds    ~ THE LENGTH THE AUDIO FILE                     #
+-- #  length : THE millisecond LENGTH OF THE SOUND FILE         #
+-- #  volume : TO TURN DOWN LOUD SOUNDS AND TUNE THEM           #
+-- #  fallOff.min/max : IS THE SOUND FALLOFF RANGE;             #
+-- #  - IF PLAYERS DISTANCE IS LESS THEN MIN, THE VOLUME IS MAX #
+-- #  - IF THE PLAYER IS IN A RANGE OF MAX,                     #
+-- #  - vOLUME IS SET BY DISTANCE SCALED BY MIN TO MAX          #
+-- #                                                            #
+-- $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+-- $$$$$$$$$$$$$$$ SERVER EVENTS YOU CAN CALL $$$$$$$$$$$$$$$$$$$
+-- $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+-- #                                                            #
+-- # PlaySoundAt   loc, _min, _max, soundFile                   #
+-- # PlaySoundOn   _ent, _min, _max, soundFile                  #
+-- # PlayLoopAt    loc, _min, _max, soundFile                   #
+-- # StopLoopAt    loc                                          #
+-- # PlayLoopFor   loc, _min, _max, soundFile, loopLength       #
+-- # PlayLoopOn    _ent, _min, _max, soundFile                  #
+-- # StopLoopOn    _ent                                         #
+-- # PlayLoopOnFor _ent, _min, _max, soundFile, loopLength      #
+-- # StopLoopAll                                                #
+-- #                                                            #
+-- ##############################################################
+
+_sounds = {
+  bankAlarm = {
+    length = 11980,
+    volume = 0.1,
+    fallOff = {
+      min = 10,
+      max = 50
+    }
+  },
+  item_chips = {
+    length = 1320,
+    volume = 1.0,
+    fallOff = {
+      min = 1,
+      max = 5
+    }
+  },
+  item_cuffs = {
+    length = 1700,
+    volume = 1.0,
+    fallOff = {
+      min = 1,
+      max = 5
+    }
+  },
+  ambient_farm = {
+    length = 16620,
+    volume = 0.5,
+    fallOff = {
+      min = 5,
+      max = 50
+    }
+  },
+  ambient_hawk = {
+    length = 3700,
+    volume = 1.0,
+    fallOff = {
+      min = 5,
+      max = 100
+    }
+  },
+  ambient_siren = {
+    length = 7500,
+    volume = 0.7,
+    fallOff = {
+      min = 10,
+      max = 50
+    }
+  }
+}
+
 local sounds = {}
 
 function _addSound(loc,_minn,_maxx,_sound,_length,_loop)
@@ -7,8 +86,46 @@ function _addSound(loc,_minn,_maxx,_sound,_length,_loop)
   local _max = _maxx
   local length = _length
   local loop = _loop
-  table.insert(sounds,{ID = math.floor(location.x*100)..math.floor(location.y*100),playing = false,location = location,_min = _min,_max = _max,sound = sound,length = length,loop = _loop,startTime = 0,played = false})
+  table.insert(sounds,{
+    ID = math.floor(location.x*100)..math.floor(location.y*100),
+    firtStart = GetGameTimer(),
+    playing = false,
+    location = location,
+    _min = _min,
+    _max = _max,
+    sound = sound,
+    length = length,
+    loop = _loop,
+    loopLength = -1,
+    startTime = GetGameTimer(),
+    played = false
+  })
 end
+
+function _addLoop(loc,_minn,_maxx,_sound,_length,_loop,_loopLength)
+  local location = loc
+  local sound = _sound
+  local _min = _minn
+  local _max = _maxx
+  local length = _length
+  local loop = _loop
+  local loopLength = _loopLength
+  table.insert(sounds,{
+    ID = math.floor(location.x*100)..math.floor(location.y*100),
+    firtStart = GetGameTimer(),
+    playing = false,
+    location = location,
+    _min = _min,
+    _max = _max,
+    sound = sound,
+    length = length,
+    loop = loop,
+    loopLength = loopLength,
+    startTime = GetGameTimer(),
+    played = false
+  })
+end
+
 function _addSoundOn(_entt,_minn,_maxx,_sound,_length,_loop)
   local _enttc = GetEntityCoords(_entt)
   local sound = _sound
@@ -16,88 +133,173 @@ function _addSoundOn(_entt,_minn,_maxx,_sound,_length,_loop)
   local _max = _maxx
   local length = _length
   local loop = _loop
-  table.insert(sounds,{ID = math.floor(_enttc.x*100)..math.floor(_enttc.y*100),playing = false,location = _enttc,_min = _min,_max = _max,sound = sound,length = length,loop = _loop,startTime = 0,played = false,entity = _entt})
+  table.insert(sounds,{
+    ID = _entt,
+    firtStart = GetGameTimer(),
+    playing = false,
+    location = _enttc,
+    _min = _min,
+    _max = _max,
+    sound = sound,
+    length = length,
+    loop = loop,
+    loopLength = -1,
+    startTime = GetGameTimer(),
+    played = false,
+    entity = _entt
+  })
+end
+
+function _addLoopOn(_entt,_minn,_maxx,_sound,_length,_loopLength)
+  local _enttc = GetEntityCoords(_entt)
+  local sound = _sound
+  local _min = _minn
+  local _max = _maxx
+  local length = _length
+  local loopLength = _loopLength
+  table.insert(sounds,{
+    ID = _entt,
+    firtStart = GetGameTimer(),
+    playing = false,
+    location = _enttc,
+    _min = _min,
+    _max = _max,
+    sound = sound,
+    length = length,
+    loop = true,
+    loopLength = loopLength,
+    startTime = GetGameTimer(),
+    played = false,
+    entity = _entt
+  })
 end
 
 Citizen.CreateThread(function()
-  local volume
+  local volume = 0
   local wasPlaying = false
   local lastVol = {}
+  local _lastUpdate = GetGameTimer()
   while true do
     local lc = GetEntityCoords(GetPlayerPed(PlayerId()))
     for k,v in pairs(sounds) do
-      if not v.playing and not v.played then
+
+      local dist  = Vdist(lc.x, lc.y, lc.z, v.location.x, v.location.y, v.location.z)
+
+      if not v.playing and not v.played and not (dist > v._max) then
         if v.entity ~= nil then
           v.location = GetEntityCoords(v.entity)
+          dist  = Vdist(lc.x, lc.y, lc.z, v.location.x, v.location.y, v.location.z)
         end
-        local dist  = Vdist(lc.x, lc.y, lc.z, v.location.x, v.location.y, v.location.z)
 
         if dist < v._min then
           volume = 1.0
         elseif dist > v._max then
           volume = 0.0
         else
-          volume =  math.floor(100 - ((dist - v._min)*100)/(v._max - v._min))/100
+          volume = math.floor(100 - ((dist - v._min)*100)/(v._max - v._min))/100
         end
-        SendNUIMessage({
-            playerID = '' .. v.ID,
-            transactionType     = 'playSound',
-            transactionFile     = v.sound,
-            volume   = volume * _volumes[v.sound]
-        })
+
+        if (GetGameTimer() - v.startTime <= 50) and (GetGameTimer() - v.startTime < v.length) then
+          SendNUIMessage({
+              playerID = '' .. v.ID,
+              transactionType     = 'playSound',
+              transactionFile     = v.sound,
+              volume   = volume * _sounds[v.sound].volume
+          })
+        else
+          TriggerServerEvent('dbug', 'PlaySound|SoundSeek|' .. v.ID ..'| ' .. math.floor(dist) .. ' away, ran: ' .. (GetGameTimer() - v.startTime))
+
+          SendNUIMessage({
+              playerID = '' .. v.ID,
+              transactionType     = 'seek',
+              transactionFile     = v.sound,
+              seek     = GetGameTimer() - v.startTime,
+              volume   = volume * _sounds[v.sound].volume
+          })
+        end
+
         lastVol[v.ID] = volume
-        v.startTime = GetGameTimer()
         v.playing = true
         v.played = true
       end
+
+
       if v.playing then
-        if GetGameTimer() - v.startTime <= v.length then
+
+        if GetGameTimer() - v.startTime <= v.length and (GetGameTimer() - _lastUpdate) > 50 then
           if v.entity ~= nil then
             v.location = GetEntityCoords(v.entity)
+            dist  = Vdist(lc.x, lc.y, lc.z, v.location.x, v.location.y, v.location.z)
           end
-          local dist  = Vdist(lc.x, lc.y, lc.z, v.location.x, v.location.y, v.location.z)
           if dist < v._min then
-            volume = 1.0
+            volume = 1.0 * _sounds[v.sound].volume
           elseif dist > v._max then
-            volume = 0.0
+            volume = 0.0 * _sounds[v.sound].volume
           else
-            volume =  math.floor(100 - ((dist - v._min)*100)/(v._max - v._min))/100
-          end
-          volume = volume * _volumes[v.sound]
-
-          --drawTxt(0.4, 0.14, 1.0,1.0,0.4, math.floor(dist) .. ' away, volume: ' .. volume, 255, 255, 255, 255)
-          if volume ~= lastVol[v.ID] and not (dist > v._max) then
-            SendNUIMessage({
-                playerID = '' .. v.ID,
-                transactionType     = 'setVolume',
-                volume   = volume
-            })
-            lastVol[v.ID] = volume
+            volume =  math.floor(100 - ((dist - v._min)*100)/(v._max - v._min))/100 * _sounds[v.sound].volume
           end
 
-        else
+          if volume ~= lastVol[v.ID] then
+            if (dist > v._max) then
+              SendNUIMessage({
+                  playerID = '' .. v.ID,
+                  transactionType     = 'setVolume',
+                  volume   = 0
+              })
+              lastVol[v.ID] = 0
+            else
+              SendNUIMessage({
+                  playerID = '' .. v.ID,
+                  transactionType     = 'setVolume',
+                  volume   = volume
+              })
+              lastVol[v.ID] = volume
+            end
+          end
+
+          _lastUpdate = GetGameTimer()
+
+        elseif (GetGameTimer() - v.startTime > v.length) then
           v.playing = false
         end
-      elseif v.playing == false and v.played then
-        if v.loop then
-          v.playing = false
-          v.played = false
-          v.startTime = GetGameTimer()
-          TriggerServerEvent('dbug', 'PlaySound:SoundsLoop:RESTART: ' .. v.startTime)
-        else
-          TriggerServerEvent('dbug', 'PlaySound:SoundsLoop:KillSound:IN ' ..GetGameTimer() - v.startTime .. ', ' .. v.length)
+      elseif v.loop then
+          if (GetGameTimer() - v.startTime) > v.length or v.loopLength == -1 then
+            if v.loopLength ~= -1 and (GetGameTimer() - v.firtStart) > v.loopLength then
+              TriggerServerEvent('dbug', 'PlaySound|SoundsLoop|KillSound|IN| ' .. v.loopLength .. ', ' .. v.length)
+              v.playing = false
+              SendNUIMessage({
+                  playerID = '' .. v.ID,
+                  transactionType     = 'stop'
+              })
+              table.remove(sounds,k)
+              TriggerServerEvent('dbug', 'PlaySound|SoundsLoop|KillSound|OUT| ' .. #sounds)
+            elseif (GetGameTimer() - v.startTime) > v.length then
+              v.playing = false
+              v.played = false
+              v.startTime = GetGameTimer()
+              TriggerServerEvent('dbug', 'PlaySound|SoundsLoop|RESTART| ' .. v.startTime)
+            end
+          end
+      else
+        if (GetGameTimer() - v.startTime) > v.length then
+          TriggerServerEvent('dbug', 'PlaySound|NoLoop|KillSound|IN| ' ..GetGameTimer() - v.startTime .. ', ' .. v.length)
           v.playing = false
           SendNUIMessage({
               playerID = '' .. v.ID,
               transactionType     = 'stop'
           })
           table.remove(sounds,k)
-          TriggerServerEvent('dbug', 'PlaySound:SoundsLoop:KillSound:OUT ' .. #sounds)
+          TriggerServerEvent('dbug', 'PlaySound|NoLoop|KillSound|OUT| ' .. #sounds)
         end
       end
+      DrawText3D(v.location, math.floor(dist) .. ' away; Volume: ' .. tonumber(volume) .. '; Time ran: ' .. GetGameTimer() - v.startTime .. '; Loop: ' .. tostring(v.loop))
     end
     Citizen.Wait(10)
   end
+end)
+
+RegisterCommand('bank',  function(source, args)
+  TriggerEvent("bankstartalarm", "blainecounty")
 end)
 
 RegisterCommand('play',  function(source, args)
@@ -111,72 +313,101 @@ RegisterCommand('play',  function(source, args)
           txt = txt .. k ..'.'
         end
       end
-      TriggerEvent("chatMessage", "", {190,190,190},txt) -- Displaying the emotes in grey
-      TriggerEvent("chatMessage", "", {255,0,0}, "Plays a sounds on your end at your location to see the diffrent sounds.")
+      TriggerEvent("chatMessage", "", {190,190,190},txt)
+      TriggerEvent("chatMessage", "", {190,235,235}, "Plays a sounds on your end at your location to see the diffrent sounds.")
     else
-      TriggerEvent('PlaySoundAt',GetEntityCoords(PlayerPedId()),5,10,args[1])
+      for k,v in pairs(_sounds) do
+        if args[1] == k then
+          TriggerServerEvent('dbug', 'PlaySound|COMMAND|play : ' .. args[1])
+          _addSound(GetEntityCoords(PlayerPedId()), v.fallOff.min, v.fallOff.max, args[1], v.length, false)
+          return
+        end
+      end
+      TriggerEvent("chatMessage", "", {255,0,0}, "Sound not found! {CASE SENSITIVE}")
     end
   end
 end)
 
-_sounds = {
-  bankAlarm = 11980,
-  item_chips = 1320,
-  item_cuffs = 1700,
-  ambient_farm = 16620,
-  ambient_hawk = 3700,
-  ambient_siren = 7500
-}
-_volumes = {
-  bankAlarm = 0.7,
-  item_chips = 1.0,
-  item_cuffs = 1.0,
-  ambient_farm = 0.5,
-  ambient_hawk = 1.0,
-  ambient_siren = 0.7
-}
+RegisterNetEvent('PlaySoundAt')
+AddEventHandler('PlaySoundAt', function(loc, _min, _max, soundFile)
+  for k,v in pairs(_sounds) do
+    if soundFile == k then
+      _addSound(loc, _min, _max, soundFile, v.length, false)
+      return
+    end
+  end
+end)
+
+RegisterNetEvent('PlaySoundOn')
+AddEventHandler('PlaySoundOn', function(_ent, _min, _max, soundFile)
+  for k,v in pairs(_sounds) do
+    if soundFile == k then
+      _addSoundOn(_ent, _min, _max, soundFile, v.length, false)
+      return
+    end
+  end
+end)
 
 RegisterNetEvent('PlayLoopAt')
 AddEventHandler('PlayLoopAt', function(loc, _min, _max, soundFile)
   for k,v in pairs(_sounds) do
     if soundFile == k then
-      TriggerServerEvent('dbug', 'PlayLoopAt| ' .. math.floor(loc.x*100)..', '..math.floor(loc.y*100))
-      _addSound(loc, _min, _max, soundFile, v, true)
+      _addSound(loc, _min, _max, soundFile, v.length, true)
+      return
     end
   end
-end)
-
-RegisterNetEvent('PlayLoopOn')
-AddEventHandler('PlayLoopOn', function(_ent, _min, _max, soundFile)
-  for k,v in pairs(_sounds) do
-    if soundFile == k then
-      TriggerServerEvent('dbug', 'PlayLoopOn| ' .. _ent)
-      _addSoundOn(_ent, _min, _max, soundFile, v, true)
-    end
-  end
-end)
-
-RegisterNetEvent('PlayLoopFor')
-AddEventHandler('PlayLoopFor', function(loc, _min, _max, soundFile, loopLength)
-      for k,v in pairs(_sounds) do
-        if soundFile == k then
-          TriggerServerEvent('dbug', 'PlayLoopAt| ' .. math.floor(loc.x*100)..', '..math.floor(loc.y*100))
-          _addSound(loc, _min, _max, soundFile, v, true)
-        end
-      end
-      Citizen.CreateThread(function()
-      Citizen.Wait(loopLength)
-      TriggerServerEvent('dbug','PlayLoopFor|STOP| ' .. math.floor(loc.x*100)..', '..math.floor(loc.y*100))
-      TriggerEvent('StopLoopAt', loc)
-    end)
 end)
 
 RegisterNetEvent('StopLoopAt')
 AddEventHandler('StopLoopAt', function(loc)
   for k,v in pairs(sounds) do
     if v.location == loc then
-      TriggerServerEvent('dbug','StopLoopAt| ' .. math.floor(loc.x*100)..', '..math.floor(loc.y*100)..', '.. v.ID)
       v.loop = false
+      return
+    end
+  end
+end)
+
+-- PLAYLOOPFOR loopLength == how long to loop for
+
+RegisterNetEvent('PlayLoopFor')
+AddEventHandler('PlayLoopFor', function(loc, _min, _max, soundFile, loopLength)
+  for k,v in pairs(_sounds) do
+    if soundFile == k then
+      _addLoop(loc, _min, _max, soundFile, v.length, true, loopLength)
+      return
+    end
+  end
+end)
+
+-- PLAYLOOPON loopLength == how long to loop for
+
+RegisterNetEvent('PlayLoopOn')
+AddEventHandler('PlayLoopOn', function(_ent, _min, _max, soundFile)
+  for k,v in pairs(_sounds) do
+    if soundFile == k then
+      _addSoundOn(_ent, _min, _max, soundFile, v.length, true)
+      return
+    end
+  end
+end)
+
+RegisterNetEvent('StopLoopOn')
+AddEventHandler('StopLoopOn', function(_ent)
+  for k,v in pairs(sounds) do
+    if v.entity == _ent then
+      v.loop = false
+      return
+    end
+  end
+end)
+
+RegisterNetEvent('PlayLoopOnFor')
+AddEventHandler('PlayLoopOnFor', function(_ent, _min, _max, soundFile, loopLength)
+  for k,v in pairs(_sounds) do
+    if soundFile == k then
+      _addLoopOn(_ent, _min, _max, soundFile, v.length, loopLength)
+      return
     end
   end
 end)
@@ -184,44 +415,22 @@ end)
 RegisterNetEvent('StopLoopAll')
 AddEventHandler('StopLoopAll', function()
   for k,v in pairs(sounds) do
-      TriggerServerEvent('dbug','StopLoop| ' .. v.ID)
       v.startTime = GetGameTimer()
       v.loop = false
   end
 end)
 
-RegisterNetEvent('PlaySoundAt')
-AddEventHandler('PlaySoundAt', function(loc, _min, _max, soundFile, soundLength)
-  for k,v in pairs(_sounds) do
-    if soundFile == k then
-      TriggerServerEvent('dbug', 'PlaySoundAt| ' .. math.floor(loc.x*100)..', '..math.floor(loc.y*100))
-      _addSound(loc, _min, _max, soundFile, v, false)
-    end
-  end
-end)
+function DrawText3D(coords, text)
+    local onScreen, _x, _y = World3dToScreen2d(coords.x, coords.y, coords.z)
 
-RegisterNetEvent('PlaySoundFor')
-AddEventHandler('PlaySoundFor', function(loc, _min, _max, soundFile, soundLength, loopLength)
-  for k,v in pairs(_sounds) do
-    if soundFile == k then
-      TriggerServerEvent('dbug', 'PlaySoundFor| ' .. math.floor(loc.x*100)..', '..math.floor(loc.y*100))
-      _addSound(loc, _min, _max, soundFile, v, true)
-    end
-  end
-end)
-
-function drawTxt(x,y ,width,height,scale, text, r,g,b,a, outline)
-    SetTextFont(0)
-    SetTextProportional(0)
-    SetTextScale(scale, scale)
-    SetTextColour(r, g, b, a)
-    SetTextDropShadow(0, 0, 0, 0,255)
-    SetTextEdge(1, 0, 0, 0, 255)
-    SetTextDropShadow()
-    if(outline)then
-	    SetTextOutline()
-	end
+    SetTextScale(0.4, 0.4)
+    SetTextFont(4)
+    SetTextProportional(1)
     SetTextEntry("STRING")
+    SetTextCentre(1)
+    SetTextColour(255, 10, 10, 255)
+    SetTextOutline()
+
     AddTextComponentString(text)
-    DrawText(x, y+ 0.005)
+    DrawText(_x, _y)
 end
