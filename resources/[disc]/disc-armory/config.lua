fd6461727b422af8aa9e55bb6a721401c2e28d08
DudeRock <50227534+LoveFistRP@users.noreply.github.com>
2019-09-30 22:29:46 -0300

    Disc Base + Armory

---
+++ resources/[disc]/disc-armory/config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.Armories = {
+    {
+        name = 'Flightschool',
+        coords = vector3(-1005.08, -2927.79, 13.99),
+        job = 'flightschool',
+        weapons = {
+            { name = 'GADGET_PARACHUTE', price = 500 },
+       
+        }
+    }
+}
+
