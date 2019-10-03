3004085ee64ea91c456a662fd7604593f28c84b5
DudeRock <50227534+LoveFistRP@users.noreply.github.com>
2019-10-02 22:14:01 -0300

    TimeTrial Teleport

--- resources/[Racing]/Timetrials/timetrials_sv.lua
+++ resources/[Racing]/Timetrials/timetrials_sv.lua
@@ -43,9 +43,10 @@ end)
 
 -- Save score and send chat message when player finishes
 RegisterServerEvent('racePlayerFinished')
-AddEventHandler('racePlayerFinished', function(message, title, newScore)
+AddEventHandler('racePlayerFinished', function(source, message, title, newScore)
     -- Get top car score for this race
-    local _src = source
+    local msgAppend = ""
+    local msgSource = source
     local msgColor = color_finish
     local allScores = getScores()
     local raceScores = allScores[title]
@@ -56,19 +57,28 @@ AddEventHandler('racePlayerFinished', function(message, title, newScore)
         if topScore == nil or newScore.time < topScore.time then
             -- Set new high score
             topScore = newScore
+            
             -- Set message parameters to send to all players for high score
+            msgSource = -1
+            msgAppend = " (fastest)"
             msgColor = color_highscore
-            TriggerClientEvent('chatMessage', _src, "[RACE]", msgColor, message .. " (fastest)")
-        else
-          TriggerClientEvent('chatMessage', _src, "[RACE]", msgColor, message)
         end
         raceScores[carName] = topScore
     else
         -- No scores for this race, create struct and set new high score
         raceScores = {}
         raceScores[newScore.car] = newScore
+        
+        -- Set message parameters to send to all players for high score
+        msgSource = -1
+        msgAppend = " (fastest)"
+        msgColor = color_highscore
     end
+    
     -- Save and store scores back to file
     allScores[title] = raceScores
     saveScores(allScores)
+    
+    -- Trigger message to all players
+    TriggerClientEvent('chatMessage', -1, "[RACE]", msgColor, message .. msgAppend)
 end)
