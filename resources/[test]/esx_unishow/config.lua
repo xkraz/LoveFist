84bed2870c4af31488566c6899878e4f2008bab1
LoveFistRP <lovefistrp@gmail.com>
2019-10-07 19:17:42 -0700

    Better Strippers Dawg

---
+++ resources/[test]/esx_unishow/config.lua
@@ -0,0 +1,64 @@
+Config = {}
+
+Config.Ped = {
+
+  {label='Stripper Female 1',type=5, hash='s_f_y_stripper_01'},
+  {label='Stripper Female 2',type=5, hash='s_f_y_stripper_02'},
+  {label='Stripper Female Fat',type=5, hash='a_f_m_fatcult_01'},
+  {label='Stripper Male 1',type=5, hash='a_m_y_musclbeac_01'},
+  {label='Stripper Male BDSM',type=5, hash='a_m_m_acult_01'},
+}
+
+Config.Salle = {
+  scene1 = {
+    label = 'Back Left Stage',
+    pos = {x=101.884, y=-1289.77, z=29.258, a=290.598},
+  },
+  scene2 = {
+    label = 'Back Right Stage',
+    pos = {x=104.6779, y=-1295.2697, z=29.258, a=297.289},
+  },
+  scene3 = {
+    label = 'Back Center Stage',
+    pos = {x=104.0453, y=-1292.199, z=29.258, a=298.912},
+  },
+  scene4 = {
+    label = 'Center Stage',
+    pos = {x=107.359, y=-1290.2869, z=28.8587, a=297.33},
+  },
+  scene5 = {
+    label = 'Front Left Stage',
+    pos = {x=112.0371 ,y=-1286.2375, z=28.4586, a=30.04},
+  },
+  scene6 = {
+    label = 'Front Right Stage',
+    pos = {x=113.205, y=-1288.293, z=28.4586, a=211.88},
+  }
+}
+
+Config.Dict = {
+  show1={
+    label = 'Lap dance 1',
+    name = 'mini@strip_club@lap_dance@ld_girl_a_song_a_p1',
+    anim ='ld_girl_a_song_a_p1_f',
+  },
+  show2={
+    label = 'Lap dance 2',
+    name = 'mini@strip_club@lap_dance@ld_girl_a_song_a_p2',
+    anim ='ld_girl_a_song_a_p2_f',
+  },
+  show3={
+    label = 'Lap dance 3',
+    name = 'mini@strip_club@lap_dance@ld_girl_a_song_a_p3',
+    anim ='ld_girl_a_song_a_p3_f',
+  },
+  -- show4={
+    -- label = 'pole_dance',
+    -- name = 'mini@strip_club@pole_dance@pole_dance1',
+    -- anim ='pd_dance_0',
+  -- },
+}
+Config.Zones = {
+
+      Pos = { x = 122.35705566406, y = -1281.4152832031, z = 29.480518341064},
+}
