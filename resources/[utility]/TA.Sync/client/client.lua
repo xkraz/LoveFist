c472d599a3c4f7d4de969569e42d64b79af9c673
DudeRock <50227534+LoveFistRP@users.noreply.github.com>
2019-10-08 00:27:45 -0300

    No trains pls

--- resources/[utility]/TA.Sync/client/client.lua
+++ resources/[utility]/TA.Sync/client/client.lua
@@ -78,11 +78,11 @@ Citizen.CreateThread(function()
 	end
 end)
 
-Citizen.CreateThread(function()
-  SwitchTrainTrack(0, true)
-  SwitchTrainTrack(3, true)
-  N_0x21973bbf8d17edfa(0, 120000)
-  SetRandomTrains(true)
-end)
+-- Citizen.CreateThread(function()
+  -- SwitchTrainTrack(0, true)
+  -- SwitchTrainTrack(3, true)
+  -- N_0x21973bbf8d17edfa(0, 120000)
+  -- SetRandomTrains(true)
+-- end)
 
 ------------------------------------------------------------------------------
